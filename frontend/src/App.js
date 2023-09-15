5672a2d8b2dda7344dbd6e38e1077ef8a27616b8
Mohammad Adnan <112101028@iitpkd.ac.in>
2023-09-15 15:16:30 +0530

    created dir for comp and styles

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Navbar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        {/* Your content goes here */}
+      
+      </main>
+    </div>
+  );
+}
+
+export default App;
