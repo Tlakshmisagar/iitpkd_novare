150a36cdcaab7177e553dcb3d29b2fcf987310d1
Mohammad Adnan <112101028@iitpkd.ac.in>
2023-09-30 01:14:08 +0530

    few changes in articles and article card added

--- frontend/src/components/Articles.js
+++ frontend/src/components/Articles.js
@@ -1,37 +1,13 @@
+import ArticleCard from "./ArticleCard";
 export default function Articles() {
   return (
     <div className="z-2 position-relative bg-white">
       <div className="container position-relative">
         <div className="pt-3"></div>
-        <div class="card mb-3 mt-5">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img src=" https://drive.google.com/uc? export=view&id=1EUZ4I0REAdP2RSCq0fVGAJpN7GFTZjGK" class="img-fluid rounded-start" alt="..."/>
-            </div>
-            <div class="col-md-8 align-self-center">
-              <div class="card-body">
-                <h5 class="card-title">Article: 1</h5>
-                <p class="card-text">Why being fit matters for trekking ?</p>
-                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="card mb-3 mt-5">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img src="..." class="img-fluid rounded-start" alt="..."/>
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-          </div>
+        <ArticleCard/>
+        <ArticleCard/>
+        <ArticleCard/>
         </div>
       </div>
-    </div>
   );
 }
--- frontend/src/components/Trails.js
+++ frontend/src/components/Trails.js
@@ -8,7 +8,7 @@ export default function Trails() {
             <TrailCard picurl="https://drive.google.com/uc? export=view&id=19-hUqrzWS4s49myhyry7PRbu2mJ6obF7" title="Dhoni Waterfalls Trek"/>
             <TrailCard picurl="https://drive.google.com/uc? export=view&id=1U0ThE56RcQrWHkjFWyhhu_kTzXAWsLVf" title="Chimmony Waterfalls Trek"/>
             <TrailCard picurl="https://drive.google.com/uc? export=view&id=1516hno68CAeJoE5mgcWmW8iZH3zdZfeu" title="Munnar"/>
-            <TrailCard picurl="https://drive.google.com/uc? export=view&id=19-hUqrzWS4s49myhyry7PRbu2mJ6obF7" title="Nelliyampathy"/>
+            <TrailCard picurl="https://lh5.googleusercontent.com/p/AF1QipPgdrLV5mKrSPjqQ1Ro5peoR0SSKD2MOxd1ZKdT=w540-h312-n-k-no" title="Nelliyampathy"/>
           </div>
         </div>
       </div>
