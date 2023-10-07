8ca07964f9d7bb7a86e203d56c892af2ec812b2c
T.Lakshmi Sagar <112101048@smail.iitpkd.ac.in>
2023-10-08 05:11:22 +0530

    modified about us

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -6,22 +6,23 @@ export default function Contact() {
   return (
     <div className="">
       <img
-         className="img-fluid position-fixed" 
-         src={Nature} 
+        className="img-fluid position-fixed"
+        src={Nature}
         //  src="https://drive.google.com/file/d/1ug4PnX-bfpzuCK_eQNEkkgKLBFnyE7-Z/view?usp=sharing"
         //  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY6X47X9LAM5y5rpDl_qCk3OK4xoExFp2Q3Q&usqp=CAU"
-         alt="something" />
+        alt="something"
+      />
       <div className="z-2 position-relative">
         <div className="py-4"></div>
         <div className="text-white text-center container">
-          <p className="display-3" style={{ paddingTop: 240 }}>
+          <p className="display-3 " style={{ paddingTop: 240 }}>
             Contact Us
           </p>
-          <p className="m-auto" style={{ paddingBottom: 120 }}>
-            For any content partnerships and queries, feel free to contact Us{" "}
+          <p className="m-auto fw-light" style={{ paddingBottom: 120 }}>
+            For any content partnerships and queries, feel free to contact Us
           </p>
         </div>
-        <div className="bg-white" style={{paddingTop:50 , paddingBottom: 50}}>
+        <div className="bg-white" style={{ paddingTop: 50, paddingBottom: 50 }}>
           <div class="container px-4">
             <div class="row gx-5">
               <div class="col ">
@@ -30,24 +31,16 @@ export default function Contact() {
                     href="https://instagram.com/novare_iitpkd?igshid=MzNlNGNkZWQ4Mg=="
                     className="me-4 text-reset d-flex justify-content-center" /*target="_blank" */
                   >
-                    <img
-                      className=""
-                      src={Insta}
-                      alt=""
-                    />
+                    <img className="" src={Insta} alt="" />
                   </a>
                   <h4 className="text-center">Instagram</h4>
-                  <p className="text-center">_________</p>
+                  <p className="text-center">novare_iitpkd</p>
                 </div>
               </div>
               <div class="col">
                 <div class="p-3 ">
                   <div className="d-flex justify-content-center">
-                    <img
-                      className=""
-                      src={Mail}
-                      alt=""
-                    />
+                    <img className="" src={Mail} alt="" />
                   </div>
                   <h4 className="text-center">Mail us</h4>
                   <p className="text-center">novare@iitpkd,ac.in</p>
