3c7e4b106442e58666de3e3a8f29a2bfc430b31d
T.Lakshmi Sagar <112101048@smail.iitpkd.ac.in>
2023-10-13 18:05:25 +0530

    small changes

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -3,6 +3,31 @@ import Insta from "../images/insta.png";
 import Mail from "../images/mail.png";
 
 export default function Contact() {
+  let mail_icon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="256"
+      height="256"
+      fill="currentColor"
+      class="bi bi-envelope-open"
+      viewBox="0 0 16 16"
+    >
+      <path d="M8.47 1.318a1 1 0 0 0-.94 0l-6 3.2A1 1 0 0 0 1 5.4v.817l5.75 3.45L8 8.917l1.25.75L15 6.217V5.4a1 1 0 0 0-.53-.882l-6-3.2ZM15 7.383l-4.778 2.867L15 13.117V7.383Zm-.035 6.88L8 10.082l-6.965 4.18A1 1 0 0 0 2 15h12a1 1 0 0 0 .965-.738ZM1 13.116l4.778-2.867L1 7.383v5.734ZM7.059.435a2 2 0 0 1 1.882 0l6 3.2A2 2 0 0 1 16 5.4V14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5.4a2 2 0 0 1 1.059-1.765l6-3.2Z" />
+    </svg>
+  );
+
+  let insta_icon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="256"
+      height="256"
+      fill="black"
+      class="bi bi-instagram"
+      viewBox="0 0 17 17"
+    >
+      <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
+    </svg>
+  );
   return (
     <div className="">
       <img
@@ -15,14 +40,17 @@ export default function Contact() {
       <div className="z-2 position-relative">
         <div className="py-4"></div>
         <div className="text-white text-center container">
-          <p className="display-3 " style={{ paddingTop: "4em"}}>
+          <p className="display-3 " style={{ paddingTop: "14rem" }}>
             Contact Us
           </p>
-          <p className="m-auto fw-light" style={{ paddingBottom: "7em"}}>
+          <p className="m-auto fw-light" style={{ paddingBottom: "7rem" }}>
             For any content partnerships and queries, feel free to contact Us
           </p>
         </div>
-        <div className="bg-white" style={{ paddingTop:"3em", paddingBottom: "3em" }}>
+        <div
+          className="bg-white"
+          style={{ paddingTop: "3rem", paddingBottom: "3rem" }}
+        >
           <div class="container px-4">
             <div class="row gx-5">
               <div class="col ">
@@ -31,7 +59,7 @@ export default function Contact() {
                     href="https://instagram.com/novare_iitpkd?igshid=MzNlNGNkZWQ4Mg=="
                     className="me-4 text-reset d-flex justify-content-center" /*target="_blank" */
                   >
-                    <img className="" src={Insta} alt="" />
+                    {insta_icon}
                   </a>
                   <h4 className="text-center">Instagram</h4>
                   <p className="text-center">novare_iitpkd</p>
@@ -40,7 +68,7 @@ export default function Contact() {
               <div class="col">
                 <div class="p-3 ">
                   <div className="d-flex justify-content-center">
-                    <img className="" src={Mail} alt="" />
+                    {mail_icon}
                   </div>
                   <h4 className="text-center">Mail us</h4>
                   <p className="text-center">novare@iitpkd,ac.in</p>
--- frontend/src/components/Team.js
+++ frontend/src/components/Team.js
@@ -4,12 +4,136 @@ export default function Team() {
     <div className="z-2 position-relative bg-white">
       <div className="py-4"></div>
       <div className="row row-cols-1 row-cols-md-4 g-4 text-center pt-3">
-      <ProfileCard photo="https://drive.google.com/uc? export=view&id=1tncWZ2C7CCQyAqv9gbvQ6Ewk7SrlSLTC" name="Mohammad Adnan" role="Web Developer Head" desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences." email="112101028@smail.iitpkd.ac.in" insta="insta-test"/>
-      <ProfileCard photo="" name="Mohammad Adnan" role="Web Developer Head" desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences." email="112101028@smail.iitpkd.ac.in" insta="insta-test"/>
-      <ProfileCard photo="" name="Mohammad Adnan" role="Web Developer Head" desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences." email="112101028@smail.iitpkd.ac.in" insta="insta-test"/>
-      <ProfileCard photo="" name="Mohammad Adnan" role="Web Developer Head" desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences." email="112101028@smail.iitpkd.ac.in" insta="insta-test"/>
+        <ProfileCard
+          photo=""
+          name="Dr. Vishwas V"
+          role="Faculty Mentor"
+          desc="Trails winding through woods, Lens captures nature's wonders, Hiking with purpose."
+          email="vishwas@iitpkd.ac.in"
+          insta="insta-test"
+        />
+        <ProfileCard
+          photo=""
+          name="Haripriya S"
+          role="Club Head "
+          desc="Adventures in the mountains makes us high enough to uplift our soul and cross our limits in the healthiest way."
+          email="102001009@smail.iitpkd.ac.in"
+          insta="https://instagram.com/abq1445?igshid=NGExMmI2YTkyZg=="
+        />
+        <ProfileCard
+          photo=""
+          name="Ajay Kumar Isampalli "
+          role="Trip coordinator"
+          desc="I always love to captivate tapestry of life and beauty, where Earth's wonders unfold, and serenity embraces the soul."
+          email="122001017@smail.iitpkd.ac.in"
+          insta="https://instagram.com/shiva09.ks?igshid=MzNlNGNkZWQ4Mg=="
+        />
+        <ProfileCard
+          photo=""
+          name="K.Aishwarya "
+          role="Trip coordinator"
+          desc="Trekking is an adventure that allows me to connect with nature and discovering breathtaking landscapes and fuels my sense of exploration. "
+          email="102001014@smail.iitpkd.ac.in"
+          insta="https://instagram.com/aishu_01._?igshid=MzNlNGNkZWQ4Mg=="
+        />
+        <ProfileCard
+          photo=""
+          name="Cipriyano Simoes"
+          role="Trip coordinator"
+          // modify desc
+          desc={`Trekking opens our minds and broadens our perspectives, inviting us to embrace new cultures, experiences, and connections, ultimately enriching our lives with unforgettable memories and a deeper understanding of the world. It's like "Travel nahi kiya toh kya kiya ?"`}
+          email="112101028@smail.iitpkd.ac.in"
+          insta="insta-test"
+        />
+        <ProfileCard
+          photo=""
+          name="Mohammad Adnan"
+          role="Web Developer Head"
+          desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences."
+          email="112101028@smail.iitpkd.ac.in"
+          insta="insta-test"
+        />
+        <ProfileCard
+          photo=""
+          name="Mohammad Adnan"
+          role="Web Developer Head"
+          desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences."
+          email="112101028@smail.iitpkd.ac.in"
+          insta="insta-test"
+        />
+        <ProfileCard
+          photo=""
+          name="Mohammad Adnan"
+          role="Web Developer Head"
+          desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences."
+          email="112101028@smail.iitpkd.ac.in"
+          insta="insta-test"
+        />
+        <ProfileCard
+          photo=""
+          name="Mohammad Adnan"
+          role="Web Developer Head"
+          desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences."
+          email="112101028@smail.iitpkd.ac.in"
+          insta="insta-test"
+        />
+        <ProfileCard
+          photo=""
+          name="Mohammad Adnan"
+          role="Web Developer Head"
+          desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences."
+          email="112101028@smail.iitpkd.ac.in"
+          insta="insta-test"
+        />
+        <ProfileCard
+          photo=""
+          name="Mohammad Adnan"
+          role="Web Developer Head"
+          desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences."
+          email="112101028@smail.iitpkd.ac.in"
+          insta="insta-test"
+        />
+        <ProfileCard
+          photo=""
+          name="Mohammad Adnan"
+          role="Web Developer Head"
+          desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences."
+          email="112101028@smail.iitpkd.ac.in"
+          insta="insta-test"
+        />
+        <ProfileCard
+          photo=""
+          name="Mohammad Adnan"
+          role="Web Developer Head"
+          desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences."
+          email="112101028@smail.iitpkd.ac.in"
+          insta="insta-test"
+        />
+        <ProfileCard
+          photo=""
+          name="Mohammad Adnan"
+          role="Web Developer Head"
+          desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences."
+          email="112101028@smail.iitpkd.ac.in"
+          insta="insta-test"
+        />
+        <ProfileCard
+          photo=""
+          name="Mohammad Adnan"
+          role="Web Developer Head"
+          desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences."
+          email="112101028@smail.iitpkd.ac.in"
+          insta="insta-test"
+        />
+        <ProfileCard
+          photo=""
+          name="Mohammad Adnan"
+          role="Web Developer Head"
+          desc="A web developer designs and builds websites and web applications, combining technical skills with creativity to create online experiences."
+          email="112101028@smail.iitpkd.ac.in"
+          insta="insta-test"
+        />
       </div>
-      
     </div>
   );
 }
